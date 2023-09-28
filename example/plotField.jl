b74ca59551dbba2a86f3cacce27ff8ca430d079a
Jonas Schumacher <github735@jonasschumacher.de>
2023-09-28 11:54:54 +0200

    Switch to artifact

--- example/plotField.jl
+++ example/plotField.jl
@@ -1,7 +1,11 @@
 using PyPlot
 using LinearAlgebra
+using Artifacts
 
 using MPISphericalHarmonics
 
-field = SphericalHarmonicsDefinedField(".\\MPISphericalHarmonics.jl\\FFP000COMSOLOptimized_tDesgn_t=12_r=45.h5")
+filename = joinpath(artifact"example", "FFP000COMSOLOptimized_tDesgn_t=12_r=45.h5")
+field = SphericalHarmonicsDefinedField(filename)
+
+figure()
 imshow(norm.(field[-0.02:0.001:0.02, -0.02:0.001:0.02, 0]))
